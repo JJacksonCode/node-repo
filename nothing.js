65c3c4779003e4b653fd8383ffea1ed4121b05ce
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-19 14:37:09 +0300

    little piece of code

--- nothing.js
+++ nothing.js
@@ -4,3 +4,13 @@ import React, { useState } from 'react';
 import Field from 'components/field';
 import Translate from 'components/translate';
 import Languages from 'components/language';
+
+export default function App() {
+  return (
+    <div>
+      <h3>Here is the react nothing app!
+      </h3>
+    </div>
+
+  );
+}
