b6afb3e406f84ffd4d5c7ea3b10ac7b99dac047c
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-19 16:29:28 +0300

    updated references

--- nothing.js
+++ nothing.js
@@ -1,5 +1,11 @@
 //nothing.js
 //console.log('This is the nothing.js script.')
+//modern react course on udemy https://www.udemy.com/course/react-redux/learn/lecture/32831326#overview
+//  https://github.com/JJacksonCode/node-repo/blob/main/nothing.js
+//
+//
+//
+
 import React, { useState } from 'react';
 import Field from 'components/field';
 import Translate from 'components/translate';
@@ -8,8 +14,7 @@ import Languages from 'components/language';
 export default function App() {
   return (
     <div>
-      <h3>Here is the react nothing app!
-      </h3>
+      <h3>Here is the react nothing app!</h3>
     </div>
 
   );
