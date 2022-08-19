695978b7ebeac291a46d977d94656d2a4dbbd585
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-19 14:27:00 +0300

    copying translation app example import fields

--- nothing.js
+++ nothing.js
@@ -1,2 +1,6 @@
 //nothing.js
-console.log('This is the nothing.js script.')
+//console.log('This is the nothing.js script.')
+import React, { useState } from 'react';
+import Field from 'components/field';
+import Translate from 'components/translate';
+import Languages from 'components/language';
