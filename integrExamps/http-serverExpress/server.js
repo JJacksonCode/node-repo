92abd90051c78efab1bd6943a3942607330fe5de
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-20 17:33:27 +0300

    added routes and request methods

--- integrExamps/http-serverExpress/server.js
+++ integrExamps/http-serverExpress/server.js
@@ -13,6 +13,34 @@ app.get('/', function (req, res) {
 app.get('welcome', function (req, res) {
   res.send('<h1>Welcome</h1><br><p>simple express http server @ localhost:3000</p>');
 });
+// adding some functionality
+// 1 - add a route that answers all requests, sounds bad. :/
+app.route('/article')
+.get(function (req, res) {
+  res.send('Get the article.');
+})
+.post(function (req, res) {
+  res.send('Add an article');
+})
+.put(function (req, res) {
+  res.send('Update the article');
+});
+
+// 2 - use a wildcard for a route
+// answers to : theAnyman, thebatman, thespiderman
+app.get('/the*man', function (req, res) {
+  res.send('the*man');
+});
+
+// 3 - use regex in routes
+// responds to : batboat, batlintu, batcave, batsauna -> batman is visiting Finland :))
+app.get(/bat/, function (req, res) {
+  res.send('/bat/');
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry, that route doesn't exist in this part of the multiverse. Try again, please. Have a nice day.");
+});
 
 // start the server on port 3333
 app.listen(3333, function() {
