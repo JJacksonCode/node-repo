9f34544d07c91fd68e031dfffe3e10d1dcbe4c0c
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-20 12:49:22 +0300

    a little node

---
+++ node-course/notes app/app.js
@@ -0,0 +1,6 @@
+// notes app for Node.js Developer course on Udemy
+// see repo -> git@github.com:JJacksonCode/node-repo.git
+//import React
+const fs = require('fs')
+
+fs.writeFileSync('notes.txt', 'This file written by Node.js, Sir.')
