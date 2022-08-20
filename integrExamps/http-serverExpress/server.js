b59f4c1f3b3eba6e145aa2ecb1979e6ca7a60dd7
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-20 17:00:05 +0300

    started up express server on 3333. funtimes

--- integrExamps/http-serverExpress/server.js
+++ integrExamps/http-serverExpress/server.js
@@ -1,3 +1,20 @@
+// this is code referenced from list site: https://ourcodeworld.com/articles/read/261/how-to-create-an-http-server-with-express-in-node-js
+
 // require express and instantiate
 var express = require('express');
 var app = express();
+
+// request calls root plays(localhost:3333/)
+app.get('/', function (req, res) {
+  res.send('<h1><b>My</b> simple express server for code example</h1>');
+});
+
+// on localhost:3333/welcome
+app.get('welcome', function (req, res) {
+  res.send('<h1>Welcome</h1><br><p>simple express http server @ localhost:3000</p>');
+});
+
+// start the server on port 3333
+app.listen(3333, function() {
+  console.log('Friendly unsecure server listening on 3333. :) go check it out.))');
+});
