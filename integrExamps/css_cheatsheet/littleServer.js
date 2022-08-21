e8e2a980a7aa76223886c6f3ffa5da18034c588c
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-21 11:40:11 +0300

    code

---
+++ integrExamps/css_cheatsheet/littleServer.js
@@ -0,0 +1,11 @@
+// here is another example of http server with js that can run in node v8 engine.
+// nice it listens on 1337 ;)
+// first req in http
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello whomever reads this. Thank you.');
+
+}).listen(1337, '127.0.0.1');
