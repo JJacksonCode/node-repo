be12081f7d42b93adb1569be8f5f71e748f1c5a3
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-21 11:15:42 +0300

    covering materials

--- node-course/notes app/app.js
+++ node-course/notes app/app.js
@@ -1,6 +1,8 @@
 // notes app for Node.js Developer course on Udemy
 // see repo -> git@github.com:JJacksonCode/node-repo.git
 //import React
+
 const fs = require('fs')
 
 fs.writeFileSync('notes.txt', 'This file written by Node.js, Sir.')
+fs.appendFileSync('notes.txt', 'this is a tracking id .98989898 767676')
---
+++ node-course/notes app/note-app/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const name = 'Memyself'
+console.log(name)
+function App() {
+  return (
+    <div className="App">
+
+
+
+        <h1>Atomic DogNotes</h1>
+        <h3>
+        <p>This app is a note taking app created with create-react-app for styling.
+        Node.js is used throughout when possible.</p>
+        </h3>
+        <p>
+          Edit everything :) <code>src/App.js</code> and save to reload.
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
