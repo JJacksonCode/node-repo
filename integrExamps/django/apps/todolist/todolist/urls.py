278a9f24c2ba9123812f1d6f5834c790e1d8b1b2
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-24 17:01:06 +0300

    django app todo restructure

--- integrExamps/django/apps/todolist/todolist/urls.py
+++ integrExamps/django/apps/todolist/todolist/urls.py
@@ -15,6 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
+
+#####################home_page###########################################
+    path('', views.index, name="todo"),
+    ####################give id no. item_id name or item_id=i.id ############
+    # pass item_id as primary key to remove that the todo with given id
+    path('del/<str:item_id>', views.remove, name="del"),
 
 urlpatterns = [
     path('admin/', admin.site.urls),
