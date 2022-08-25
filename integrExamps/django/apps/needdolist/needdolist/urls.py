d1ce7382f4f91c75021bbbd8bd6e5e7172997ab0
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-25 09:25:14 +0300

    today js

--- integrExamps/django/apps/needdolist/needdolist/urls.py
+++ integrExamps/django/apps/needdolist/needdolist/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
+    path('$', include('needdos.urls')),
+    path('needdos/', include('needdos.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ integrExamps/js_crashcourse/main.js
@@ -0,0 +1,11 @@
+//console.log('Hola compadres!!!');
+//console.error('Bruh, this is an error.');
+//console.warn('Let this be a warning to ya');
+// let, const
+// let reassigns vals, const is constant
+
+const age = 30; // sure bro, ha ha
+let score = 31;
+score = 41;
+console.log('Age is: ', age)
+console.log(score);
