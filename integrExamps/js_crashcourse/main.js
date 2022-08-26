df596b4b5ff764ed2bffab6f12e7476011003101
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-26 23:29:17 +0300

    code

--- integrExamps/js_crashcourse/main.js
+++ integrExamps/js_crashcourse/main.js
@@ -14,8 +14,17 @@ function Person(firstName, lastName, dob) {
   this.dob = new Date(dob);
 }
 
+Person.prototype.getBirthYear = function() {
+  return this.dob.getFullYear();
+}
+Person.prototype.getFullName = function() {
+  return `${this.firstName} ${this.lastName}`;
+}
+
 // instantiate object
 const person1 = new Person('Ricki', 'Bobby', '4-4-1984');
 const person2 = new Person('Mari', 'Joensuu', '3-3-1992');
 
-console.log(person1, person2.lastName);
+console.log(person1, person2.lastName, person2.dob);
+console.log(person1.getBirthYear(), person1.getFullName(), person2.getBirthYear(), person2.getFullName());
+console.log(person2);
