05d4a851d658d7ebd23297faf7fed3252c712839
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-26 11:45:57 +0300

    code

---
+++ integrExamps/js_crashcourse/addnums.js
@@ -0,0 +1,13 @@
+/*
+
+function addNums(num1, num2) {
+  console.log(num1 + num2);
+}
+
+*/
+
+const addNums = (a, b) => {
+  return a + b;
+}
+
+console.log(addNums(2, 31997));
--- integrExamps/js_crashcourse/main.js
+++ integrExamps/js_crashcourse/main.js
@@ -9,8 +9,10 @@ comment */
 //
 //
 
-const x = 7.5;
+const x = 71.5;
 const y = 11;
+
+
 
 if(x > 5 && y > 10) {
   console.log('x is more than 5 or y is more than 10');
@@ -19,4 +21,18 @@ if(x > 5 && y > 10) {
 }
 else {
   console.log('x is NOT 10, it might be a little less.');
+}
+
+const color = x > 10 ? 'red' : 'blue';
+
+switch (color) {
+  case 'red':
+    console.log('color is red');
+    break;
+  case 'blue':
+    console.log('this one blue');
+    break;
+  default:
+    console.log('color is Neither red NOR blue');
+    break;
 }
