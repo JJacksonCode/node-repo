b37f1cada651daea976e8568ec2ee3e9d769ec2f
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-27 15:22:22 +0300

    code

--- integrExamps/js_crashcourse/main.js
+++ integrExamps/js_crashcourse/main.js
@@ -16,7 +16,7 @@ console.log(letters);
 
 // multiple items
 console.log(document.querySelectorAll('.item'));
-comment */
+comment
 
 //console.log(window.location);
 
@@ -31,9 +31,33 @@ ul.children[1].innerText = 'Coderssons';
 ul.lastElementChild.innerHTML = '<h1>Greetings Earthlings</h1>';
 
 const btn = document.querySelector('.btn');
-btn.style.background = 'red';
+//btn.style.background = 'red';
 btn.addEventListener('click', (e) => {
   e.preventDefault();
   console.log(e.target.className);
   document.querySelector('#my-form').style.background = '#ccc';
+  document.querySelector('body').classList.add('bg-dark');
 });
+
+console.log(navigator.appName);
+
+*/
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if(nameInput.value === '' || emailInput.value === '') {
+    alert('Please give up the data!');
+  } else {
+    console.log('success');
+  }
+
+}
