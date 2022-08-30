450fc186f0f5ab651d825a1d4bedba19f65b64f6
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-30 20:08:51 +0300

    code

---
+++ integrExamps/async_js/promises.js
@@ -0,0 +1,40 @@
+// https://www.youtube.com/watch?v=PoRJizFvM7s
+// asynchronous js exmaples
+// git@github/JJacksonCode/..
+
+const posts = [
+  { title: 'Post One', body: 'This is post one'},
+  {title: 'Post Two', body: 'This is post two'}
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+        const error = true;
+
+        if(!error) {
+          resolve();
+        } else {
+          reject('Error: Something went wrong');
+        }
+    }, 2000);
+  });
+}
+
+
+
+createPost({ title: 'Post Three', body: 'This is post three.'})
+  .then(getPosts)
+  .catch(err => console.log(err));
