c2487574df71bf9c0b50c8adfc377aea96d7fd3d
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-30 19:18:42 +0300

    code

---
+++ integrExamps/async_js/async.js
@@ -0,0 +1,15 @@
+// asynchronous
+
+console.log(' I ');
+
+console.log(' eat ');
+
+
+
+console.log(' with a ');
+
+console.log(' spoon ');
+
+setTimeout(() =>{
+  console.log(' ice cream ');
+},4000);
