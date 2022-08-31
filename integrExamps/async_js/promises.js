a7b0764fd52e7efb69e07668ad7617b0f3ff4502
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2022-08-31 22:19:53 +0300

    cool json data resolves

--- integrExamps/async_js/promises.js
+++ integrExamps/async_js/promises.js
@@ -44,5 +44,6 @@ const promise1 = Promise.resolve('Hello subset of the world that has access to t
 const promise2 = 10;
 const promise3 = new Promise((resolve, reject) =>
 setTimeout(resolve, 2000, 'Adios'));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
-Promise.all([promise1, promise2, promise3]).then(values => console.log(values));
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
